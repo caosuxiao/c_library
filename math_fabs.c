60cdd8c04b7c384a6e7a7335616953c87989bbaa
caosu <4siyantianji@gmail.com>
2023-10-18 15:58:38 +0800

    modified

--- math_fabs.c
+++ math_fabs.c
@@ -5,6 +5,10 @@ void main() {
     //  double fabs(double x)
     //  雖說是double, 但也是可以傳int float, 
     //  因為sizeof(double)(8) > sizeof(float)(4) = sizeof(int)(4)
+
+    //  double fabs(double) 處理的是小數絕對值 在, math.h 裡;
+    //  int abs(int)  處理整數的絕對值, 在 stdlib 裡;
+
     int one_number_u_like;
     printf("input a number and I'll show u magic(it's better to have a negative number)\n");
     scanf("%d", &one_number_u_like);  
