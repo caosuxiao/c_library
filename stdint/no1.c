a58746d64f898c0eee99efa7152c70f3945d083d
caosu <4siyantianji@gmail.com>
2024-01-05 22:01:49 +0800

    ?

---
+++ stdint/no1.c
@@ -0,0 +1,20 @@
+/*
+define:
+
+
+signed          unsigned
+range           range
+
+int8_t          uint8_t
+-2^7 ~ 2^7-1    0 ~ 2^8-1
+
+int16_t         uint16_t
+-2^15 ~ 2^15-1  0 ~ 2^16-1
+
+int32_t         uint32_t
+-2^31 ~ 2^31-1  0 ~ 2^32-1
+
+int64_t         uint64_t
+-2^63 ~ 2^63-1  0 ~ 2^64-1
+
+*/
